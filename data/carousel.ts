13528d8210959761dea539fba18ba51c15f40f56
meraeugene <villalon.andrew123@gmail.com>
2025-04-30 11:55:56 +0800

    v8

--- data/carousel.ts
+++ data/carousel.ts
@@ -2,21 +2,21 @@ export const carouselData = [
   {
     title: "Anker Solix Solar Generator Kit",
     button: "View Details",
-    src: "https://res.cloudinary.com/dupynxkci/image/upload/v1745982967/gearflow/featured/zigew9jx6gkkkvig6hrl.webp",
+    src: "/assets/images/featured/f1.webp",
   },
   {
     title: "Apple Vision Pro",
     button: "View Details",
-    src: "https://res.cloudinary.com/dupynxkci/image/upload/v1745982968/gearflow/featured/z4gdqypprm2shsfb1b0k.webp",
+    src: "/assets/images/featured/f2.webp",
   },
   {
     title: "Meta Quest 3",
     button: "View Details",
-    src: "https://res.cloudinary.com/dupynxkci/image/upload/v1745982968/gearflow/featured/b3c4edo1damc0tqt9lv0.webp",
+    src: "/assets/images/featured/f3.webp",
   },
   {
     title: "DJI Air 3",
     button: "View Details",
-    src: "https://res.cloudinary.com/dupynxkci/image/upload/v1745982968/gearflow/featured/mdvmzhb2k7lhure6mnbt.webp",
+    src: "/assets/images/featured/f4.webp",
   },
 ];
--- data/categoryBanner.ts
+++ data/categoryBanner.ts
@@ -1,16 +1,9 @@
 export const categoryBackgrounds: Record<string, string> = {
-  books:
-    "https://res.cloudinary.com/dupynxkci/image/upload/v1745983277/gearflow/categories/fxoxgzqpl9vyghprocbh.webp",
-  electronics:
-    "https://res.cloudinary.com/dupynxkci/image/upload/v1745983277/gearflow/categories/qxxbbarouajhp8rxr4lg.webp",
-  construction:
-    "https://res.cloudinary.com/dupynxkci/image/upload/v1745983278/gearflow/categories/bxlkhsgnyl9u3mz6sbv4.webp",
-  footwear:
-    "https://res.cloudinary.com/dupynxkci/image/upload/v1745983277/gearflow/categories/enzpzd8u3eyyfvwsak2x.webp",
-  "gowns & suits":
-    "https://res.cloudinary.com/dupynxkci/image/upload/v1745983278/gearflow/categories/dir4ojmkgyndi6dy8uyc.webp",
-  "gym equipment":
-    "https://res.cloudinary.com/dupynxkci/image/upload/v1745983277/gearflow/categories/amkdozgmiomrx5jfwgep.webp",
-  outdoors:
-    "https://res.cloudinary.com/dupynxkci/image/upload/v1745983277/gearflow/categories/g70xtlzatgflkh4azfwx.webp",
+  books: "/assets/images/books/bookBG.webp",
+  electronics: "/assets/images/electronics/electronicBG.webp",
+  construction: "/assets/images/construction/constructionBG.webp",
+  footwear: "/assets/images/footwear/footwearBG.webp",
+  "gowns & suits": "/assets/images/gownsandsuits/gownandsuitBG.webp",
+  "gym equipment": "/assets/images/gym/gymequipmentBG.webp",
+  outdoors: "/assets/images/outdoor/outdoorBG.webp",
 };
