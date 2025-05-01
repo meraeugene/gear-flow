b18b170edd75583df29e72192d7579297167c9db
meraeugene <villalon.andrew123@gmail.com>
2025-05-01 12:53:02 +0800

    added suspense to lender page

--- app/(root)/lenders/[id]/[slug]/page.tsx
+++ app/(root)/lenders/[id]/[slug]/page.tsx
@@ -2,10 +2,12 @@ import { getLenderUnitsWithProfile } from "@/actions/lenderActions";
 import { AlertComponent } from "@/components/AlertComponent";
 import BackButton from "@/components/BackButton";
 import BreadcrumbNav from "@/components/BreadcrumbNav";
+import GlobalLoader from "@/components/GlobalLoader";
 import UserUnits from "@/components/UserUnits";
 import { capitalizeFirstLetter } from "@/utils/string/capitalizeFirstLetter";
 import { generateSlug } from "@/utils/string/generateSlug";
 import Image from "next/image";
+import { Suspense } from "react";
 
 const LenderPage = async ({
   params,
@@ -45,48 +47,50 @@ const LenderPage = async ({
   ];
 
   return (
-    <div className="px-24 pt-12 pb-20">
-      <div className="space-y-7">
-        <BackButton />
-        <BreadcrumbNav items={breadcrumbs} />
-      </div>
+    <Suspense fallback={<GlobalLoader />}>
+      <div className="px-24 pt-12 pb-20">
+        <div className="space-y-7">
+          <BackButton />
+          <BreadcrumbNav items={breadcrumbs} />
+        </div>
 
-      <div className="profile__container mt-12 ml-32 flex items-start gap-10">
-        <Image
-          src={owner?.profile_picture || ""}
-          width={160}
-          height={160}
-          className="rounded-full object-cover"
-          alt="profile picture"
-        />
+        <div className="profile__container mt-12 ml-32 flex items-start gap-10">
+          <Image
+            src={owner?.profile_picture || ""}
+            width={160}
+            height={160}
+            className="rounded-full object-cover"
+            alt="profile picture"
+          />
 
-        <div className="space-y-3">
-          <h1 className="text-3xl font-bold text-gray-800">
-            {capitalizeFirstLetter(owner.first_name)}{" "}
-            {capitalizeFirstLetter(owner.last_name)}
-          </h1>
+          <div className="space-y-3">
+            <h1 className="text-3xl font-bold text-gray-800">
+              {capitalizeFirstLetter(owner.first_name)}{" "}
+              {capitalizeFirstLetter(owner.last_name)}
+            </h1>
 
-          <div className="space-y-1 text-gray-600">
-            <p>
-              <span className="font-semibold">Address:</span>{" "}
-              {owner?.address || "N/A"}
-            </p>
-            <p>
-              <span className="font-semibold">Email:</span>{" "}
-              {owner?.email || "N/A"}
-            </p>
-            <p>
-              <span className="font-semibold">Phone:</span>{" "}
-              {owner?.phone_number || "N/A"}
-            </p>
+            <div className="space-y-1 text-gray-600">
+              <p>
+                <span className="font-semibold">Address:</span>{" "}
+                {owner?.address || "N/A"}
+              </p>
+              <p>
+                <span className="font-semibold">Email:</span>{" "}
+                {owner?.email || "N/A"}
+              </p>
+              <p>
+                <span className="font-semibold">Phone:</span>{" "}
+                {owner?.phone_number || "N/A"}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="user-units__container mt-14 border-t border-gray-100 pt-14">
-        <UserUnits units={userUnits ?? []} />
+        <div className="user-units__container mt-14 border-t border-gray-100 pt-14">
+          <UserUnits units={userUnits ?? []} />
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
