5e5dcc0241f26ee521362ce436454b56db82cfa0
meraeugene <villalon.andrew123@gmail.com>
2025-05-09 08:15:43 +0800

    manifest json

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/assets/images/logo.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
